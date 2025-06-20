bd4af9d77461f7902e21b3c285aa645f63b6a482
jfkarle <jfkarle@gmail.com>
2025-06-20 18:52:12 -0400

    Update ecm_scheduler_logic.py

--- ecm_scheduler_logic.py
+++ ecm_scheduler_logic.py
@@ -66,8 +66,8 @@ ECM_RAMPS = {
     "PlymouthHarbor": Ramp("PlymouthHarbor", "Plymouth Harbor", "8446493", "HoursAroundHighTide", 3.0), # VERIFIED
     "CordagePark": Ramp("CordagePark", "Cordage Park (Plymouth)", "8446493", "HoursAroundHighTide", 1.5, ["Powerboat"]), # VERIFIED
     "DuxburyHarbor": Ramp("DuxburyHarbor", "Duxbury Harbor (Town Pier)", "8446166", "HoursAroundHighTide", 1.0, ["Powerboat"]), # CORRECTED
-    "GreenHarborTaylors": Ramp("GreenHarborTaylors", "Green Harbor (Taylors)", "8445071", "HoursAroundHighTide", 3.0, ["Powerboat"]), # VERIFIED
-    "GreenHarborSafeHarbor": Ramp("GreenHarborSafeHarbor", "Safe Harbor (Green Harbor)", "8445071", "HoursAroundHighTide", 1.0, ["Powerboat"]), # VERIFIED
+    "GreenHarborTaylors": Ramp("GreenHarborTaylors", "Green Harbor (Taylors)", "8446009)", "HoursAroundHighTide", 3.0, ["Powerboat"]), # VERIFIED
+    "GreenHarborSafeHarbor": Ramp("GreenHarborSafeHarbor", "Safe Harbor (Green Harbor)", "8446009)", "HoursAroundHighTide", 1.0, ["Powerboat"]), # VERIFIED
     "ScituateHarborJericho": Ramp("ScituateHarborJericho", "Scituate Harbor (Jericho Road)", "8445138", "AnyTideWithDraftRule"), # CORRECTED
     "CohassetParkerAve": Ramp("CohassetHarbor", "Cohasset Harbor (Parker Ave)", "8444762", "HoursAroundHighTide", 3.0), # CORRECTED
     "HullASt": Ramp("HullASt", "Hull (A St, Sunset, Steamboat)", "8444199", "HoursAroundHighTide_WithDraftRule", 3.0), # CORRECTED
