f0403fae95e35f0ee6508896b9795cbafe49cd26
jfkarle <jfkarle@gmail.com>
2025-06-28 22:17:42 -0400

    Update ecm_scheduler_logic.py

--- ecm_scheduler_logic.py
+++ ecm_scheduler_logic.py
@@ -432,6 +432,10 @@ def find_available_job_slots(customer_id, boat_id, service_type, requested_date_
     global CRANE_DAY_LOGIC_ENABLED
     try:
         requested_date_obj = datetime.datetime.strptime(requested_date_str, '%Y-%m-%d').date()
+        # --- NEW: If a hard date range is being used, force the request date to be the start of that range ---
+        # This prevents the sorting logic from pulling in dates outside the desired window.
+        if hard_search_start_date:
+        requested_date_obj = hard_search_start_date
     except ValueError:
         return [], "Error: Invalid date format.", [], False
 
