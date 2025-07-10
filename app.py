2377a26fe960bb00295a45c3d701269034602065
jfkarle <jfkarle@gmail.com>
2025-07-10 14:16:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from st_supabase_connection import SupabaseConnection
 st.set_page_config(layout="wide")
 st.title("Supabase Connection Test")
 
-conn = st.connection("supabase", type=SupabaseConnection, url="postgresql://postgres:[YOUR-PASSWORD]@db.knexrzljvagiwqstapnk.supabase.co:5432/postgres", key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZXhyemxqdmFnaXdxc3RhcG5rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjA4NjQ4MiwiZXhwIjoyMDY3NjYyNDgyfQ.KpLrXdOQFYvDKq1DQ_YjLU-3yYFFqkIjFpO20dFFsk4")
+conn = st.connection("supabase", type=SupabaseConnection, url="postgresql://postgres:[YOUR-PASSWORD]@db.knexrzljvagiwqstapnk.supabase.co:5432/postgres", key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZXhyemxqdmFnaXdxc3RhcG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwODY0ODIsImV4cCI6MjA2NzY2MjQ4Mn0.hgWhtefyiEmGj5CERladOe3hMBM-rVnwMGNwrt8FT6Y")
 
 
 try:
