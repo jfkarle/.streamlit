1fc53ea21a095a630407c6851bce64a4130c93c6
jfkarle <jfkarle@gmail.com>
2025-07-10 14:20:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
+conn = st.connection("supabase", type=SupabaseConnection) 
 
 st.set_page_config(layout="wide")
 st.title("Supabase Connection Test")
-
-conn = st.connection("supabase", type=SupabaseConnection, url="postgresql://postgres:[YOUR-PASSWORD]@db.knexrzljvagiwqstapnk.supabase.co:5432/postgres", key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZXhyemxqdmFnaXdxc3RhcG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwODY0ODIsImV4cCI6MjA2NzY2MjQ4Mn0.hgWhtefyiEmGj5CERladOe3hMBM-rVnwMGNwrt8FT6Y")
 
 
 try:
