e9eac7df24aeef154c3e251f296d56220935b043
jfkarle <jfkarle@gmail.com>
2025-07-10 14:14:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from st_supabase_connection import SupabaseConnection
 st.set_page_config(layout="wide")
 st.title("Supabase Connection Test")
 
+conn = st.connection("supabase", type=SupabaseConnection, url="postgresql://postgres:[YOUR-PASSWORD]@db.knexrzljvagiwqstapnk.supabase.co:5432/postgres", key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZXhyemxqdmFnaXdxc3RhcG5rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjA4NjQ4MiwiZXhwIjoyMDY3NjYyNDgyfQ.KpLrXdOQFYvDKq1DQ_YjLU-3yYFFqkIjFpO20dFFsk4")
+
+
 try:
     # Initialize connection.
     conn = st.connection("supabase", type=SupabaseConnection)
