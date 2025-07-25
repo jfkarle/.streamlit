88b021e7e910acc4b0bd96c6f0cf2af7efdd2fab
JasonK <jfkarle@gmail.com>
2025-07-25 10:19:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ def generate_progress_report_pdf(stats, dist_analysis, eff_analysis):
     detail_table = Table(table_data, colWidths=[150, 150, 50, 150])
     detail_table.setStyle(TableStyle([
         ('BACKGROUND', (0,0), (-1,0), colors.grey),
-        ('TEXTCOLOR', (0,0), (-1,0), colors.whitesoke),
+        ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
         ('ALIGN', (0,0), (-1,-1), 'LEFT'),
         ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
         ('BOTTOMPADDING', (0,0), (-1,0), 12),
