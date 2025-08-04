f63096af71c9c36a3395dfc41abe950e37650926
JasonK <jfkarle@gmail.com>
2025-08-04 19:22:16 -0400

    Update ecm_scheduler_logic.py

--- ecm_scheduler_logic.py
+++ ecm_scheduler_logic.py
@@ -12,6 +12,14 @@ from datetime import timedelta, time, timezone
 from collections import Counter, defaultdict   # pull in defaultdict here
 from geopy.geocoders import Nominatim
 
+import os
+from supabase import create_client
+
+SUPA_URL = os.environ["https://supabase.com/dashboard/project/knexrzljvagiwqstapnk"]
+SUPA_KEY = os.environ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZXhyemxqdmFnaXdxc3RhcG5rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjA4NjQ4MiwiZXhwIjoyMDY3NjYyNDgyfQ.KpLrXdOQFYvDKq1DQ_YjLU-3yYFFqkIjFpO20dFFsk4"]
+supabase = create_client(SUPA_URL, SUPA_KEY)
+
+
 _geolocator = Nominatim(user_agent="ecm_boat_scheduler_app")
 _location_coords_cache = {} # Ensure this line is present here
 
